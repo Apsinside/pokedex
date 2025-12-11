f972c4123b8ba1669d65b4a7d3614810c1e18f43
Fabian <f.zickwolff@googlemail.com>
2025-12-11 12:15:05 +0100

    CH1: REPL | L4: REPL

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
+// repl.js actually refers to repl.ts
+import { startREPL } from "./repl.js";
+
+
+
+
+
 function main() {
-  console.log("Hello, world!");
+  startREPL();
 }
 
 main();
--- src/repl.ts
+++ src/repl.ts
@@ -4,3 +4,26 @@ export function cleanInput(input: string): string[] {
   .split(" ")
   .filter(i => i);
 }
+
+
+import { createInterface } from 'node:readline';
+import { stdin, stdout } from 'node:process';
+const rl = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex >"
+});
+
+function callback(line: string): void{
+    const words = cleanInput(line);
+    if(words.length){
+        console.log(`Your command was: ${words[0]}`);
+    }
+    rl.prompt();
+}
+
+export function startREPL() : void{
+
+    rl.prompt();
+    rl.on('line', callback);
+}
